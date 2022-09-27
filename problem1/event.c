c65057c38b83535708ffb97d3761368cf44dc625
Katsumi5617 <83553986+Katsumi5617@users.noreply.github.com>
2022-09-26 22:33:21 -0500

    Update event.c

--- problem1/event.c
+++ problem1/event.c
@@ -5,6 +5,7 @@
 Event *CreateEvent(char *name)
 {
     Event *evento=malloc(sizeof(Event));
+    name[15] = 0;
     strncpy(evento->eventName,name,strlen(name) +1);
     evento->next=NULL;
     return evento;
