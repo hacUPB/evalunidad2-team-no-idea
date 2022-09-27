dbb273f9d7d837c38b5e2e4c4ead762e8db7f119
Katsumi5617 <83553986+Katsumi5617@users.noreply.github.com>
2022-09-26 21:56:08 -0500

    Update event.c

--- problem1/event.c
+++ problem1/event.c
@@ -4,10 +4,7 @@
 
 Event *CreateEvent(char *name)
 {
-     Event *event=malloc(sizeof(Event));
-    strncpy(evento->eventName,name,strlen(name) +1);
-    event->next=NULL;
-    return evento;
+
 }
 
 void DestroyEvent(Event *this)
