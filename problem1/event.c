f917958594b0353600517d5346881a7d61011b71
SheiinX <82066007+SheiinX@users.noreply.github.com>
2022-09-26 23:06:55 -0500

    Update event.c
    pequeño intento

--- problem1/event.c
+++ problem1/event.c
@@ -4,13 +4,19 @@
 
 Event *CreateEvent(char *name)
 {  
-        
-Event *eptr = malloc(sizeof(Event));
-name[15] = 0;
-strncpy(eptr->eventName,name,strlen(name) + 1);
-eptr ->next = NULL;
-return eptr; 
-        
+        /*
+        Event *eptr = malloc(sizeof(Event));
+        name[15] = 0;
+        strncpy(eptr->eventName,name,strlen(name) + 1);
+        eptr ->next = NULL;
+        return eptr; 
+        */
+        Event *event;
+        char nameE;
+        name = &nameE;
+        event = malloc(sizeof(Event));
+        event ->eventName[0] = nameE;
+        return event;
 }
 
 
