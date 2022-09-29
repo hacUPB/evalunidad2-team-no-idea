634d61030719016e67ce0f071868fa6a21f5218c
Nivekke <110250498+Nivekke@users.noreply.github.com>
2022-09-29 15:12:23 -0500

    Update event.c

--- problem1/event.c
+++ problem1/event.c
@@ -5,11 +5,10 @@
 Event *CreateEvent(char *name)
 {  
         
-Event *eptr = malloc(sizeof(Event));
-name[15] = 0;
-strncpy(eptr->eventName,name,strlen(name) + 1);
-eptr ->next = NULL;
-return eptr; 
+Event *ptr = malloc(sizeof(Event ));
+        
+   strcpy(ptr->eventName, name);
+   ptr->next = NULL;
         
 }
 
