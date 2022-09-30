7dc731a85febd6e028e2b4d009eaa30eb34db830
SheiinX <82066007+SheiinX@users.noreply.github.com>
2022-09-29 20:17:32 -0500

    AddEvent change

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -42,6 +42,8 @@ void AddEvent(EventList *this, Event *event)
 {
     Event *aux = this->last;
     //Usar la función de buscar para que no agregue un event existente
+    Event *evenT = SearchEvent(this, event);
+    if(strcmp(event->eventName, eventT->eventName) == 0){
     if(this->isEmpty == 0){
         this->head = event;
         this->last = event;
@@ -49,6 +51,7 @@ void AddEvent(EventList *this, Event *event)
     }else{
         aux->next = event;
         this->last = event;
+        }
     }
 }
 
